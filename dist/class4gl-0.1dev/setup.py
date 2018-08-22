fa082ef05f7430587faf2eb14facdfaec1d95a97
hendrikwout <hendrikwout@gmail.com>
2018-08-22 17:04:44 +0200

    cleanups

--- dist/class4gl-0.1dev/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-        name='class4gl',
-        version='0.1dev',
-        packages=['lib','bin'],
-        license='GPLv3 licence',
-        long_description=open('README.md').read(),
-)
